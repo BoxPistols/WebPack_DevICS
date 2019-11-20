098aa554503ba239c723c240290ea58931b08831
ait <ait@ex.com>
2019-11-20 21:49:29 +0900

    set sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import 文を使って sub.js ファイルを読み込む。
 import { hello } from "./sub";
 import { dev } from "./dev";
-import "./style.css";
+import "./style.sass";
 
 // sub.jsに定義されたJavaScriptを実行する。
 hello();
--- webpack.config.js
+++ webpack.config.js
@@ -23,27 +23,34 @@ module.exports = {
     open: true
   },
   module: {
-    rules: [
-      // CSSファイルの読み込み
-      {
-        // 対象となるファイルの拡張子
-        test: /\.css/,
-        // ローダー名
-        use: [
-          // linkタグに出力する機能
-          "style-loader",
-          // CSSをバンドルするための機能
-          {
-            loader: "css-loader",
-            options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
-              url: false,
-              // ソースマップを有効にする
-              sourceMap: enabledSourceMap
-            }
+    rules: [{
+      test: /\.sass/, // 対象となるファイルの拡張子
+      use: [
+        // linkタグに出力する機能
+        "style-loader",
+        // CSSをバンドルするための機能
+        {
+          loader: "css-loader",
+          options: {
+            // オプションでCSS内のurl()メソッドの取り込みを禁止する
+            url: false,
+            // ソースマップの利用有無
+            sourceMap: enabledSourceMap,
+
+            // 0 => no loaders (default);
+            // 1 => postcss-loader;
+            // 2 => postcss-loader, sass-loader
+            importLoaders: 2
           }
-        ]
-      }
-    ]
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            // ソースマップの利用有無
+            sourceMap: enabledSourceMap
+          }
+        }
+      ]
+    }]
   }
 };
