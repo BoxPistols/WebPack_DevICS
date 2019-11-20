0e9e03e70c09d8b13faa7ea8ef79c7fe63c56a04
ait <ait@ex.com>
2019-11-20 22:30:55 +0900

    Postcss AutoPrefix

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const AutoPrefixer = require('autoprefixer');
+
 // 'production' か 'development' を指定
 const MODE = "development";
 // ソースマップの利用有無(productionのときはソースマップを利用しない)
@@ -41,6 +43,19 @@ module.exports = {
             // 1 => postcss-loader;
             // 2 => postcss-loader, sass-loader
             importLoaders: 2
+          }
+        }, {
+          loader: "postcss-loader",
+          options: {
+            // PostCSS側でもソースマップを有効にする
+            sourceMap: true,
+            plugins: [
+              // Autoprefixerを有効化
+              // ベンダープレフィックスを自動付与する
+              require("autoprefixer")({
+                grid: true
+              })
+            ]
           }
         },
         {
