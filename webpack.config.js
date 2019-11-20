eb0b7ad48609b5bd0004de80403435ce3d4c6034
ait <ait@ex.com>
2019-11-20 21:38:52 +0900

    ad css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+// 'production' か 'development' を指定
+const MODE = "development";
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = MODE === "development";
+
 module.exports = {
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: `./src/index.js`,
 
-  mode: "development", // production
+  mode: MODE,
+  // mode: "production", // development or production
 
   // // ファイルの出力設定
   output: {
@@ -15,5 +21,29 @@ module.exports = {
   devServer: {
     contentBase: "dist",
     open: true
+  },
+  module: {
+    rules: [
+      // CSSファイルの読み込み
+      {
+        // 対象となるファイルの拡張子
+        test: /\.css/,
+        // ローダー名
+        use: [
+          // linkタグに出力する機能
+          "style-loader",
+          // CSSをバンドルするための機能
+          {
+            loader: "css-loader",
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
+              // ソースマップを有効にする
+              sourceMap: enabledSourceMap
+            }
+          }
+        ]
+      }
+    ]
   }
 };
